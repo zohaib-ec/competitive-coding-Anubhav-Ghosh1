6a7e52d33d926377218765801e388fb10cb1f7ac
Anubhav Ghosh <88537612+Anubhav-Ghosh1@users.noreply.github.com>
2023-03-25 12:33:09 +0530

    Update TwoFourWheeler.c

--- TwoFourWheeler.c
+++ TwoFourWheeler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
     int two_wheelers, four_wheelers;
     int total_vehicles, total_wheels;
@@ -6,7 +7,8 @@ int main() {
     scanf("%d %d", &total_vehicles, &total_wheels); //do not change this line
 
     //Write your code here
-
+    two_wheelers = abs(2*total_vehicles - total_wheels)/2;
+    four_wheelers = abs(total_vehicles - two_wheelers);
     printf("%d %d\n", four_wheelers, two_wheelers); //do not change this line
   
 }
